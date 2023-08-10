4ca59c56dcdc90b7a998aef0c920cf67600336f6
Vanessa Carvalhal <108913207+Carvalhal1509@users.noreply.github.com>
2023-08-10 14:19:06 -0300

    Add files via upload

---
+++ ufbaprojeto/src/ufbaprojeto/Main.java
@@ -0,0 +1,12 @@
+package ufbaprojeto;
+
+class Main {
+	  public static void main(String[] args) {
+	      Triangulo t = new Triangulo (2,2,2);
+	   
+	      System.out.println("Triangulo lado A=" + t.getA());
+
+	      System.out.println("Olá mundo!");
+	   
+	  }
+	}
